fc8fe8171ff66235fb987f4cfa60db449e7e65e1
2-MK <1124muzammil@gmail.com>
2026-08-05 13:27:58 +0530

    fixed the view issue

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vaisa\Desktop\CC versions\CC V3\CoreConnect\venv
+set VIRTUAL_ENV=C:\26\test\CoreConnect\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
